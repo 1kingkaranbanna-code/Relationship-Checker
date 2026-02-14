6fbb7404d1305fcccb6913747a7d03dc6ae98cf6
Karan Singh <1king.karanbanna@gmail.com>
2026-02-13 16:00:14 -0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,24 @@
+# relationship
+def relationship(): 
+    name=input("\nenter your name:")
+    a="married"
+    b="mingle"
+    c="single"
+    d="i am not interested in love life"
+    while True:
+        relationship_status=input("\nchoose your relationship status:\n(a)merried\n(b)mingle\n(c)single\n(d)i am not intrested in love life\nyour ans=",)
+        if(relationship_status=="a"):
+            print(name,"You are married. Wishing you a happy life!")
+            break
+        elif(relationship_status=="b"):
+            print(name,"Enjoying your love life 😄")
+            break
+        elif(relationship_status=="c"):
+            print(name,"Single life is peaceful and focused!")
+            break
+        elif(relationship_status=="d"):
+            print(name,"Full focus on goals. Respect!")
+            break
+        else:
+            print(name,"❌ Invalid choice! Please select only (a, b, c, d).")
+relationship()
